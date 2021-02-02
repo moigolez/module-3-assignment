4fef3d8e25914bf7ad86a7603bc80a0b5a227aac
moises golez <mgolez7185@southwestuniversity.edu>
2021-02-02 10:55:29 -0700

    This is my module 9 (test)

--- Modules/Program.cs
+++ Modules/Program.cs
@@ -12,31 +12,7 @@ namespace Modules
 
         static void Main()
         {
-            List<int> myList = new List<int>();
-
-         
-        
-
-
-            myList.Add(1);
-            myList.Add(2);
-            myList.Add(3);
-            myList.Add(4);
-            myList.Add(5);
-            myList.Add(6);
-            myList.Add(7);
-            myList.Add(8);
-            myList.Add(9);
-            myList.Add(10);
-
-
-
-
-            int result = myList.Select(number => number).Last();
-
-          
-
-            Console.WriteLine(result);
+            int variable = 0;
            
         }
          
