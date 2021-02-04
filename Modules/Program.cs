32a10610e9d1734da736070030cc20321fa1ec88
moises golez <mgolez7185@southwestuniversity.edu>
2021-02-04 10:04:51 -0700

    This is module 5 assignment

--- Modules/Program.cs
+++ Modules/Program.cs
@@ -8,14 +8,23 @@ namespace Modules
 {
     class Program
     {
-        private static object role;
+        
 
         static void Main()
         {
-            int variable = 0;
-            Console.WriteLine(variable);
+            int result = 0;
 
-            //test
+            for(int x=1;x<21;x++)
+            {
+
+                result = result + x;
+          }
+
+
+
+            Console.WriteLine(result);
+
+            
         }
          
 
