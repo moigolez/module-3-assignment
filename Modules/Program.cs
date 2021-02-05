4e79ed2e554b1e97da84dcff5154d4071eeeef53
moises golez <mgolez7185@southwestuniversity.edu>
2021-02-05 00:29:28 -0700

    This is module 6 assignment

--- Modules/Program.cs
+++ Modules/Program.cs
@@ -8,31 +8,53 @@ namespace Modules
 {
     class Program
     {
-        
+
 
         static void Main()
         {
-            DateTime myDateTimeVariable = new DateTime(2010, 05, 10);
-            DateTime myDateTimeVariable2 = new DateTime(1998, 01, 01);
+            int[] myArray = new int[10];
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 5;
+            myArray[5] = 6;
+            myArray[6] = 7;
+            myArray[7] = 8;
+            myArray[8] = 9;
+            myArray[9] = 10;
 
-            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
 
-            
-
-            Console.WriteLine(result);
+            Console.WriteLine("The values on the array are the following:");
+            foreach (var item in myArray)
 
 
             {
+                Console.Write(item + ",");
 
-                
-          }
+            }
 
 
+            List<string> myList = new List<string>();
+            myList.Add("Value 1");
+            myList.Add("Value 2");
+            myList.Add("Value 3");
+            myList.Add("Value 4");
+            myList.Add("Value 5");
+            myList.Add("Value 6");
+            myList.Add("Value 7");
+            myList.Add("Value 8");
+            myList.Add("Value 9");
+            myList.Add("Value 10");
 
-           
-            
+            Console.WriteLine("");
+            Console.WriteLine("The values on the list are the following:");
+            foreach (var item in myList)
+
+            {
+
+                Console.Write(item + ",");
+            }
         }
-         
-
     }
 }
