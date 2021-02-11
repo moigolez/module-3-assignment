e1394c38bd989ad122e9765a05746ab123f465f0
moises golez <mgolez7185@southwestuniversity.edu>
2021-02-10 19:42:50 -0700

    This is the midterm project

--- Modules/Program.cs
+++ Modules/Program.cs
@@ -16,27 +16,82 @@ namespace Modules
 
         static void Main()
         {
+            string response = "";
+            List<User> userList = new List<User>();
+            List<Comment> commentList = new List<Comment>();
 
-            string path = "file.txt";
 
-            bool fileExist = File.Exists(path);
-
-            if(fileExist)
+            while (response != "5")
             {
-                Console.WriteLine("File already exists");
-            }
-            else
-            {
-                Console.WriteLine("You have created the file: " +
-                                  path);
-                File.Create(path);
+                Console.WriteLine("1. Create a User");
+                Console.WriteLine("2. Create a Comment");
+                Console.WriteLine("3. Print the List of Users");
+                Console.WriteLine("4. Print the List of Comments");
+                Console.WriteLine("5. Exit");
 
-            }
-           
+                Console.WriteLine("");
+                Console.WriteLine("Please select an option");
+                response = Console.ReadLine();
 
-            
+                switch(response)
+                {
+                    case "1":
+                        User user = new User();
+                        user.Email = "kimi1@yahoo.com";
+                        user.Password = "Password";
+                        user.UserId = 1;
+                        user.UserName = "Kimi";
+
+                        userList.Add(user);
+
+                        Console.WriteLine("You have created a user");
+                        break;
+
+                    case "2":
+                        Comment myComment = new Comment();
+                        myComment.CommentId = 1;
+                        myComment.TheComment = "Hello";
+                       
+
+                        commentList.Add(myComment);
+
+                        Console.WriteLine("You have created a comment");
+
+                        break;
+
+                    case "3":
+                        Console.WriteLine("These is the list of users created:");
+
+                        foreach(var insideUser in userList)
+                        {
+
+                            Console.WriteLine("The user email is: " + insideUser.Email);
+                            Console.WriteLine("The user password is: " + insideUser.Password);
+                            Console.WriteLine("The user user id is: " + insideUser.UserId);
+                            Console.WriteLine("The user user name is: " + insideUser.UserName);
+
+                        }
+                        break;
+
+                    case "4":
+                        Console.WriteLine("These is the list of comments created:");
+
+                        foreach (var insideComment in commentList)
+                        {
+
+                            Console.WriteLine("The  comment id is: " + insideComment.CommentId);
+                            Console.WriteLine("The actual comment is: " + insideComment.TheComment);
+                          
+                        }
+                        break;
+                }
+            }
+
+
+
+
         }
-        
+
     }
 }
 
